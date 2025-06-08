5865381a2f666faf7d83629d21eb302ab4b6299d
DeadlyMcBrick <camilorietveld2024@gmail.com>
2025-06-08 18:43:31 -0500

    Add project files.

---
+++ OpenGL2.0/Main.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+void processInput(GLFWwindow* window)
+{
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, true);
+}
+
+int main()
+{
+	glfwInit();
+	glfwInitHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwInitHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwInitHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	GLFWwindow* window = glfwCreateWindow(800, 900, "DevOpengl", NULL, NULL);
+	if (window == NULL)
+	{
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+
+	glfwMakeContextCurrent(window);
+	gladLoadGL();
+
+	//Viewport Specified
+	glViewport(0, 0, 800, 800);
+
+	//Change the color background
+	glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
+	glClear(GL_COLOR_BUFFER_BIT);
+	glfwSwapBuffers(window);
+
+	while (!glfwWindowShouldClose(window))
+	{
+		//input
+		processInput(window);
+		glfwPollEvents();
+	}
+
+	glfwDestroyWindow(window);
+	glfwTerminate();
+	return 0;
+}
